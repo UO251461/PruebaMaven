2aa164e413fd330632b6aef50f03ff636407c4f9
Manu <Manu@192.168.1.35>
2017-10-11 18:05:59 +0200

    añadido carpeta lib con el .jar de oracle para conectarse

--- Proyecto/src/database/Base.java
+++ Proyecto/src/database/Base.java
@@ -1,13 +1,24 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Base {
+	private static final String CONNECTION_STRING = "jdbc:hsqldb:hsql://localhost/labdb";
+	
 	private static final String USERNAME="IPS_2018_5";
 	private static final String PASSWORD="hola2018";
 	
 	private BaseCarreras baseCarrera;
 	
 	
-	
+	public static Connection getConection() throws SQLException{
+		if(DriverManager.getDriver(CONNECTION_STRING)==null)
+			if(CONNECTION_STRING.contains("oracle"))
+				DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+		return DriverManager.getConnection(CONNECTION_STRING,USERNAME,PASSWORD);
+	}
 	
 	public Base(){
 		inicializar();
