4099705e66d65c027f0fdd8a4d1cc2a17f8e782d
Manu <Manu@192.168.1.35>
2017-10-12 12:15:57 +0200

    Añadido metodo para consultar en la base los datos de la tabla de inscipciones de un determinado atleta

--- Proyecto/src/database/Base.java
+++ Proyecto/src/database/Base.java
@@ -1,8 +1,7 @@
 package database;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Base {
 	private static final String CONNECTION_STRING = "jdbc:oracle:thin:@156.35.94.99:1521:desa";
@@ -30,6 +29,35 @@ public class Base {
 	
 	public BaseCarreras getBaseCarrera(){
 		return baseCarrera;
+	}
+	
+	/**
+	 * Devuelve la informacion del atleta de todas las carreras
+	 * muestra el estado de su inscripcion
+	 * tambien el tiempo y la posicion si dicha carrera ha finalizado.
+	 * @param atleta identificador del atleta cuyos datos se desean obtener.
+	 */
+	public void getDatosAtleta(String atleta) {
+		try {
+			Connection con = getConection();
+			PreparedStatement ps=con.prepareStatement("");	//<-- aqui va la consulta que esta en proceso de crearse
+			ps.setString(1, atleta);	//se introduce el identificador del atleta
+			ResultSet rs = ps.executeQuery();		
+			
+			while(rs.next()) {
+				//mientras se reciban datos, se almacenaran en un array para posteriormente tratarlos
+				//en la logica o donde sea necesario
+				//por lo que este metodo en vez de void devolvera un Array
+			}
+			
+			rs.close();
+			ps.close();
+			con.close();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
