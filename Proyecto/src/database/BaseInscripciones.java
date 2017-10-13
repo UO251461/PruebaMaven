04d99380f7d3a5b422bf5f7d5f0f6efce4086609
grandaju <uo244759@uniovi.es>
2017-10-13 19:23:46 +0200

    añadiendo funcioniladad a la clase

--- Proyecto/src/database/BaseInscripciones.java
+++ Proyecto/src/database/BaseInscripciones.java
@@ -11,4 +11,9 @@ public class BaseInscripciones {
 	}
 	
 	private ArrayList<Inscripcion> inscripciones;
+	
+	
+	public static void cambiarEstado(String estado) {
+		//metodo que llama a la base de datos y cambia por consulta el estado de la inscripcion por uno que se le pase
+	}
 }
