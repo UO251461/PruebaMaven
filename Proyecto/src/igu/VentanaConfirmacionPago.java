4dc1b78f91e46f3264cff89a34edd71cd3d1be59
grandaju <uo244759@uniovi.es>
2017-10-13 19:11:23 +0200

    Cambiada la clase VentanaConfirmacion

--- Proyecto/src/igu/VentanaConfirmacionPago.java
+++ Proyecto/src/igu/VentanaConfirmacionPago.java
@@ -13,81 +13,110 @@ import java.awt.Font;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import database.*;
 
 public class VentanaConfirmacionPago extends JDialog {
 
+	
+	private VentanaMetodoPago vi;
 	private final JPanel contentPanel = new JPanel();
-	private JTextField txtNumCuenta;
+	private JLabel lblCuenta;
+	private JTextField txtEntidadBancaria;
+	private JLabel lblPrecio;
 	private JTextField txtPrecio;
 	private JButton btnAceptar;
+	
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		try {
-			VentanaConfirmacionPago dialog = new VentanaConfirmacionPago();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	/**
+//	 * Launch the application.
+//	 */
+//	public static void main(String[] args) {
+//		try {
+//			VentanaConfirmacionPago dialog = new VentanaConfirmacionPago();
+//			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+//			dialog.setVisible(true);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 	/**
 	 * Create the dialog.
 	 */
-	public VentanaConfirmacionPago() {
-		setBounds(100, 100, 498, 317);
+	public VentanaConfirmacionPago (VentanaMetodoPago vi) {
+		this.vi = vi;
+		setBounds(100, 100, 593, 317);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBackground(Color.WHITE);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
+		contentPanel.add(getLblCuenta());
+		contentPanel.add(getTxtEntidadBancaria());
+		contentPanel.add(getLblPrecio());
+		contentPanel.add(getTxtPrecio());
+		contentPanel.add(getBtnAceptar());
 		
-		JLabel lblCuentaBancaria = new JLabel("Cuenta bancaria: ");
-		lblCuentaBancaria.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblCuentaBancaria.setBounds(26, 32, 147, 27);
-		contentPanel.add(lblCuentaBancaria);
-		
-		txtNumCuenta = new JTextField();
-		txtNumCuenta.setBackground(Color.WHITE);
-		txtNumCuenta.setFont(new Font("Tahoma", Font.PLAIN, 21));
-		txtNumCuenta.setText("ES-6543232101301030501");
-		txtNumCuenta.setEditable(false);
-		txtNumCuenta.setBounds(149, 29, 281, 27);
-		contentPanel.add(txtNumCuenta);
-		txtNumCuenta.setColumns(10);
-		
-		JLabel lblPrecioInscripcin = new JLabel("Precio inscripci\u00F3n: ");
-		lblPrecioInscripcin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblPrecioInscripcin.setBounds(26, 94, 117, 19);
-		contentPanel.add(lblPrecioInscripcin);
-		
-		txtPrecio = new JTextField();
-		txtPrecio.setBackground(Color.WHITE);
-		txtPrecio.setDisabledTextColor(Color.WHITE);
-		txtPrecio.setEditable(false);
-		txtPrecio.setBounds(149, 95, 117, 20);
-		contentPanel.add(txtPrecio);
-		txtPrecio.setColumns(10);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setBackground(Color.WHITE);
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			buttonPane.add(getBtnAceptar());
-		}
 	}
+	private JLabel getLblCuenta() {
+		if (lblCuenta == null) {
+			lblCuenta = new JLabel("Cuenta Bancaria: ");
+			lblCuenta.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			lblCuenta.setBounds(23, 36, 124, 25);
+		}
+		return lblCuenta;
+	}
+	private JTextField getTxtEntidadBancaria() {
+		if (txtEntidadBancaria == null) {
+			txtEntidadBancaria = new JTextField();
+			txtEntidadBancaria.setBackground(Color.WHITE);
+			txtEntidadBancaria.setEditable(false);
+			txtEntidadBancaria.setFont(new Font("Tahoma", Font.PLAIN, 20));
+			txtEntidadBancaria.setBounds(170, 31, 381, 31);
+			txtEntidadBancaria.setColumns(10);
+		}
+		return txtEntidadBancaria;
+	}
+	private JLabel getLblPrecio() {
+		if (lblPrecio == null) {
+			lblPrecio = new JLabel("Precio inscripci\u00F3n: ");
+			lblPrecio.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			lblPrecio.setBounds(23, 141, 139, 25);
+		}
+		return lblPrecio;
+	}
+	private JTextField getTxtPrecio() {
+		if (txtPrecio == null) {
+			txtPrecio = new JTextField();
+			txtPrecio.setBounds(170, 140, 139, 31);
+			txtPrecio.setColumns(10);
+		}
+		return txtPrecio;
+	}
+	
+	private void setTextEntidad(String entidad) {
+		this.txtEntidadBancaria.setText(entidad);
+	}
+	
 	private JButton getBtnAceptar() {
 		if (btnAceptar == null) {
 			btnAceptar = new JButton("Aceptar");
 			btnAceptar.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0) {
 					dispose();
+					vi.dispose();
+					//cerrar las demas ventanas que se relacionen con el pago
+					pasarAPendientePago();
 				}
 			});
+			btnAceptar.setBackground(Color.WHITE);
+			btnAceptar.setBounds(459, 233, 89, 23);
 		}
 		return btnAceptar;
+	}
+	
+	private void pasarAPendientePago() {
+		//este metodo pone el estado de la inscripcion a "Pendiente de Pago"
+		BaseInscripciones.cambiarEstado("Pendiente de Pago");
 	}
 }
