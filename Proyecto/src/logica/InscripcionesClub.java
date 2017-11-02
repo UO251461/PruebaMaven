b57dcc9b71a604a04491023997fcfc579da68780
Juan Granda Molaguero <uo244759@uniovi.es>
2017-11-02 15:54:22 +0100

    Merge branch 'Modificacion_Ventana_Corredores'

---
+++ Proyecto/src/logica/InscripcionesClub.java
@@ -0,0 +1,45 @@
+package logica;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class InscripcionesClub {
+
+	ArrayList<Inscripcion> inscripciones;
+
+	public InscripcionesClub(String fichero) {
+		inscripciones = leerFichero(fichero);
+	}
+
+	private ArrayList<Inscripcion> leerFichero(String fichero) {
+		BufferedReader br = null;
+		ArrayList<Inscripcion> ins = new ArrayList<Inscripcion>();
+		try {
+
+			br = new BufferedReader(new FileReader(fichero));
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				// ins.add(new Inscripcion(fields[0],field[1],field[2],field[3],"PRE-INSCRITO")) //creo una inscripcion y la añado
+				line = br.readLine();
+			}
+
+			br.close();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Ha habido un error al cargar el fichero");
+		}
+		
+		return ins;
+
+	}
+	
+	
+	public ArrayList<Inscripcion> getInscripcionesClub(){
+		return this.inscripciones;
+	}
+	
+	
+}
