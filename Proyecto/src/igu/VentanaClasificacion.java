a20996286b1658c0fa27a04d4703556a6c557727
Manuel Junco <UO252010@uniovi.es>
2017-11-26 12:40:03 +0100

    correcion error que motraba una tabla null

--- Proyecto/src/igu/VentanaClasificacion.java
+++ Proyecto/src/igu/VentanaClasificacion.java
@@ -36,32 +36,32 @@ public class VentanaClasificacion extends JDialog {
 	private ArrayList<String> categorias;
 	private int sexo;
 
-//	/**
-//	 * Launch the application.
-//	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					VentanaClasificacion frame = new VentanaClasificacion("6");
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
+	// /**
+	// * Launch the application.
+	// */
+	// public static void main(String[] args) {
+	// EventQueue.invokeLater(new Runnable() {
+	// public void run() {
+	// try {
+	// VentanaClasificacion frame = new VentanaClasificacion("6");
+	// frame.setVisible(true);
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
+	// });
+	// }
 
 	/**
 	 * Create the frame.
 	 */
 	public VentanaClasificacion(Carrera carrera, ArrayList<String> categorias, int sexo) {
 		setTitle("Clasificacion");
-		base=new Base();
+		base = new Base();
 		base.inicializar();
-		this.carrera=carrera;
-		this.categorias=categorias;
-		this.sexo=sexo;
+		this.carrera = carrera;
+		this.categorias = categorias;
+		this.sexo = sexo;
 		setBounds(100, 100, 1042, 658);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -72,29 +72,28 @@ public class VentanaClasificacion extends JDialog {
 		contentPane.add(getBtnGenerarFichero(), BorderLayout.SOUTH);
 		generarTablas();
 	}
-	
-	public void añadirFilas(String categoria){
+
+	public void añadirFilas(String categoria) {
 		ArrayList<Inscripcion> datos;
 		Object[] nuevaFila = new Object[6];
 		datos = base.getBaseInscripciones().generarClasificaciones(sexo, carrera.getIdcarrera(), categoria);
-		int pos=1;
-		for(Inscripcion i:datos){
-			if(true){
-			nuevaFila[0]=pos++;
-			nuevaFila[1]= i.getCorredor().getDni();
-			nuevaFila[2]= i.getDorsal();
-			if(i.getTiempo()==0)
-				nuevaFila[3]="---";
-			else
-				nuevaFila[3]=i.getTiempo();
-			nuevaFila[4]=i.getCorredor().getSexo();
-			nuevaFila[5]=i.getCategoria();
-			modeloTabla.addRow(nuevaFila);
+		int pos = 1;
+		for (Inscripcion i : datos) {
+			if (true) {
+				nuevaFila[0] = pos++;
+				nuevaFila[1] = i.getCorredor().getDni();
+				nuevaFila[2] = i.getDorsal();
+				if (i.getTiempo() == 0)
+					nuevaFila[3] = "---";
+				else
+					nuevaFila[3] = i.getTiempo();
+				nuevaFila[4] = i.getCorredor().getSexo();
+				nuevaFila[5] = i.getCategoria();
+				modeloTabla.addRow(nuevaFila);
 			}
 		}
 	}
-	
-	
+
 	private JScrollPane getScrollPane_1() {
 		if (scrollPane == null) {
 			scrollPane = new JScrollPane();
@@ -102,6 +101,7 @@ public class VentanaClasificacion extends JDialog {
 		}
 		return scrollPane;
 	}
+
 	private JPanel getPanel_3() {
 		if (panelCentral == null) {
 			panelCentral = new JPanel();
@@ -109,31 +109,32 @@ public class VentanaClasificacion extends JDialog {
 		}
 		return panelCentral;
 	}
+
 	private JLabel getLblNombreCarrera() {
 		if (lblNombreCarrera == null) {
 			lblNombreCarrera = new JLabel(carrera.getNombre());
 		}
 		return lblNombreCarrera;
 	}
+
 	private JButton getBtnGenerarFichero() {
 		if (btnGenerarFichero == null) {
 			btnGenerarFichero = new JButton("Generar Fichero");
 		}
 		return btnGenerarFichero;
 	}
-	
-	
-	private void generarTablas(){
+
+	private void generarTablas() {
 		panelCentral.removeAll();
-		if(categorias.contains("Todos")){
-			categorias=new ArrayList<String>();
+		if (categorias.contains("Todos")) {
+			categorias = new ArrayList<String>();
 			categorias.add("Generales");
 			ArrayList<Categoria> cat = carrera.getCategorias();
-			for(Categoria c:cat)
-				if(!categorias.contains(c.getCategoria()))
+			for (Categoria c : cat)
+				if (!categorias.contains(c.getCategoria()))
 					categorias.add(c.getCategoria());
 		}
-		for(String categoria: categorias){			
+		for (String categoria : categorias) {
 			panelCentral.add(crearPanelTabla(categoria));
 		}
 	}
@@ -155,36 +156,44 @@ public class VentanaClasificacion extends JDialog {
 	private JTable crearTabla(String categoria) {
 		JTable tablaClasificacion;
 		ModeloNoEditable modeloTabla;
-		if(categoria.equals("Generales")){
-		tablaClasificacion = new JTable();
-		String[] nombreColumnas = {"Posicion","dni","Dorsal", "Tiempo","Sexo","Categoria"};
-		modeloTabla = new ModeloNoEditable(nombreColumnas,0); //al pasarle bnombre de columnas creara el numero necesario... filas 0 ya que
-		//las crearemos en tiempo de ejecucion
-		tablaClasificacion.setModel(modeloTabla);
-		//Aplica el renderer que marca la fila seleccionada en rojo y negrita
-		//tablaInscripciones.setDefaultRenderer(Object.class, new RendererSubstance());
-		///subo el alto de la fila
-		tablaClasificacion.setRowHeight(30);
-		//modifico el ancho de la columna 0
-		tablaClasificacion.getTableHeader().getColumnModel().getColumn(0).setPreferredWidth(50);
-		tablaClasificacion.getTableHeader().setReorderingAllowed(false);
-		añadirFilas(categoria);		
-	}else{
-		tablaClasificacion = new JTable();
-		String[] nombreColumnas = {"Posicion","dni","Dorsal", "Tiempo","Sexo"};
-		modeloTabla = new ModeloNoEditable(nombreColumnas,0); //al pasarle bnombre de columnas creara el numero necesario... filas 0 ya que
-		//las crearemos en tiempo de ejecucion
-		tablaClasificacion.setModel(modeloTabla);
-		//Aplica el renderer que marca la fila seleccionada en rojo y negrita
-		//tablaInscripciones.setDefaultRenderer(Object.class, new RendererSubstance());
-		///subo el alto de la fila
-		tablaClasificacion.setRowHeight(30);
-		//modifico el ancho de la columna 0
-		tablaClasificacion.getTableHeader().getColumnModel().getColumn(0).setPreferredWidth(50);
-		tablaClasificacion.getTableHeader().setReorderingAllowed(false);
-		añadirFilas(categoria);
-	}
-		return null;
+		if (categoria.equals("Generales")) {
+			tablaClasificacion = new JTable();
+			String[] nombreColumnas = { "Posicion", "dni", "Dorsal", "Tiempo", "Sexo", "Categoria" };
+			modeloTabla = new ModeloNoEditable(nombreColumnas, 0);
+			// al pasarle bnombre de columnas creara el numero necesario...
+			// filas 0 ya que
+			// las crearemos en tiempo de ejecucion
+			tablaClasificacion.setModel(modeloTabla);
+			// Aplica el renderer que marca la fila seleccionada en rojo y
+			// negrita
+			// tablaInscripciones.setDefaultRenderer(Object.class, new
+			// RendererSubstance());
+			/// subo el alto de la fila
+			tablaClasificacion.setRowHeight(30);
+			// modifico el ancho de la columna 0
+			tablaClasificacion.getTableHeader().getColumnModel().getColumn(0).setPreferredWidth(50);
+			tablaClasificacion.getTableHeader().setReorderingAllowed(false);
+			añadirFilas(categoria);
+		} else {
+			tablaClasificacion = new JTable();
+			String[] nombreColumnas = { "Posicion", "dni", "Dorsal", "Tiempo", "Sexo" };
+			modeloTabla = new ModeloNoEditable(nombreColumnas, 0);
+			// al pasarle bnombre de columnas creara el numero necesario...
+			// filas 0 ya que
+			// las crearemos en tiempo de ejecucion
+			tablaClasificacion.setModel(modeloTabla);
+			// Aplica el renderer que marca la fila seleccionada en rojo y
+			// negrita
+			// tablaInscripciones.setDefaultRenderer(Object.class, new
+			// RendererSubstance());
+			/// subo el alto de la fila
+			tablaClasificacion.setRowHeight(30);
+			// modifico el ancho de la columna 0
+			tablaClasificacion.getTableHeader().getColumnModel().getColumn(0).setPreferredWidth(50);
+			tablaClasificacion.getTableHeader().setReorderingAllowed(false);
+			añadirFilas(categoria);
+		}
+		return tablaClasificacion;
 	}
 
 	private JPanel crearPanelTitulo(String categoria) {
