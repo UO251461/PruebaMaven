2d556adde4a2b13f5bcf93b7e84e1dc2c96637ff
Manuel Junco <UO252010@uniovi.es>
2017-11-28 16:04:29 +0100

    quitados constructores no usados

--- Proyecto/src/igu/VentanaClasificacion.java
+++ Proyecto/src/igu/VentanaClasificacion.java
@@ -42,21 +42,6 @@ public class VentanaClasificacion extends JDialog {
 	private ArrayList<String> categorias;
 	private int sexo;
 
-	// /**
-	// * Launch the application.
-	// */
-	// public static void main(String[] args) {
-	// EventQueue.invokeLater(new Runnable() {
-	// public void run() {
-	// try {
-	// VentanaClasificacion frame = new VentanaClasificacion("6");
-	// frame.setVisible(true);
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// }
-	// }
-	// });
-	// }
 
 	/**
 	 * Create the frame.
